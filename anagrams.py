b05b9acd0459f4636700e7bae4fa7a412ebb81b7
clayt <clayt>
2015-10-28 22:08:40 -0500

    implemented node class

--- anagrams.py
+++ anagrams.py
@@ -5,66 +5,104 @@ Created on Oct 26, 2015
 @author: templetonc
 '''
 
+class Node():
+    def __init__(self, forward={}, back=None, word=None):
+        self.forward = forward
+        self.back = back
+        self.word = word
+        
+    def __repr__(self):
+        return "Node({}, {}, {})".format(self.forward, self.back, self.word)
+    
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
+
 class AnagramDictionary():
     def __init__(self, path=None):
-        self.node_list = [{}]
-        self.words = [None]
-        self.back_pointers = [None]
+        self.start_node = Node()
+        self.node_list = [self.start_node]
         self.anagrams = []
         if path:
             self.set_dictionary(path)
                 
     def set_dictionary(self, path, additive=False):
         if additive == False:
-            while len(self.node_list) > 1 : self.node_list.pop()
-            while len(self.words) > 1: self.words.pop()
-            while len(self.words) > 1: self.back_pointers.pop()
+            self.clear_dictionary() 
         with open(path, 'rb') as infile:
                 for line in infile:
-                	self._insert_word(line.strip())
-                        
+                    word = line.strip()
+                    if self._is_alphabetical(word):
+                        self._insert_word(word)
+    
+    def clear_dictionary(self):
+        while len(self.node_list) > 1 : self.node_list.pop()
+        self.start_node.forward.clear()
+                
+    def _is_alphabetical(self, word):
+        alphabetical = True
+        letters = [letter.name for letter in Alphabet]
+        for letter in word:
+            if letter not in letters:
+                alphabetical = False
+        return alphabetical
+    
     def _insert_word(self, word, word_pos=0, array_pos=0):
         if word_pos == len(word):
-            self.words[array_pos] = word
+            print "word = {}".format(word)
+            self.node_list[array_pos].word = word
         else:
             letter = word[word_pos]
-            if letter in self.node_list[array_pos]:
-                self._insert_word(word, word_pos + 1, self.node_list[array_pos][letter])
+            if letter in self.node_list[array_pos].forward:
+                print ("letter = {}. recursively calling _insert_word".format(letter))
+                self._insert_word(word, word_pos + 1, self.node_list[array_pos].forward[letter])
             else:
                 for letter in word[word_pos:len(word) - 1]:
-                    self.node_list[array_pos][letter] = len(self.node_list)
-                    self.back_pointers.append(array_pos)
+                    print "word_pos, array_pos, letter = {}, {}, {}".format(word_pos, array_pos, letter)
+                    self.node_list[array_pos].forward[letter] = len(self.node_list)
+                    back = array_pos
                     array_pos = len(self.node_list)
-                    self.node_list.append({})
-                    self.words.append(None)
+                    self.node_list.append(Node({}, back, None))
                 letter = word[len(word) - 1]
-                self.node_list[array_pos][letter] = len(self.node_list)
-                self.back_pointers.append(array_pos)
+                print "word_pos, array_pos, letter = {}, {}, {}".format(word_pos, array_pos, letter)
+                self.node_list[array_pos].forward[letter] = len(self.node_list)
+                back = array_pos
                 array_pos = len(self.node_list)
-                self.node_list.append({})
-                self.words.append(word)
+                self.node_list.append(Node({}, back, word))
                 
     def find_anagrams(self, phrase):           
         assert isinstance(phrase, Phrase), "Argument to find_anagrams must be of type Phrase()"
         self._backtrack(phrase, [])
         
     def _backtrack(self, phrase, solution, array_pos=0):
+        #print "backtracking. solution is {}, array_pos is {}".format(solution, array_pos)
         if self._is_solution(phrase, solution):
-            if self.words[array_pos]:
+            #print ("found a so-called solution")
+            if self.node_list[array_pos].word:
+                #print ("solution is a solution:")
                 self._process_solution(phrase, solution)
         else:
             candidates = self._construct_candidates(phrase, array_pos)
+            #print "chose candidates. Candidates are {}".format(candidates)
             for candidate in candidates:
+                #print ("iterating candidate loop. candidate is {}, array_pos is {}".format(candidate, array_pos))
                 array_pos = self._make_move(candidate, solution, array_pos, phrase)
+                #print "updated array position and chose candidate. Candidate is {}, array position is {}, solution is {}".format(candidate, array_pos, solution)
                 self._backtrack(phrase, solution, array_pos)
                 array_pos = self._unmake_move(solution, array_pos, candidate, phrase)
      
     def _construct_candidates(self, phrase, array_pos):
         candidates = []
-        for letter in self.node_list[array_pos]:
+        for letter in self.node_list[array_pos].forward:
             if phrase.dictionary[letter] > 0:
                 candidates.append(letter)
-        if self.words[array_pos]:
+        if self.node_list[array_pos].word:
             candidates.append(' ')
         return candidates
     
@@ -75,14 +113,16 @@ class AnagramDictionary():
         else:
             solution.append(candidate)
             phrase.dictionary[candidate] = phrase.dictionary[candidate] - 1
-            array_pos = self.node_list[array_pos][candidate]
+            array_pos = self.node_list[array_pos].forward[candidate]
         return array_pos
     
     def _unmake_move(self, solution, array_pos, candidate, phrase):
+        #print "unmaking move. array_pos before unmaking move is {}".format(array_pos)
         solution.pop()
-        array_pos = self.back_pointers[array_pos]
+        array_pos = self.node_list[array_pos].back
         if candidate != ' ':
             phrase.dictionary[candidate] = phrase.dictionary[candidate] + 1
+        #print "unmade move. array_pos after unmaking move is {}".format(array_pos)
         return array_pos
         
     def _is_solution(self, phrase, solution):
@@ -100,10 +140,13 @@ class AnagramDictionary():
     
     def _process_solution(self, phrase, solution):
         self.anagrams.append("".join(solution))
-        print "{} is an anagram of {}".format("".join(solution), phrase.full) 
+        print "{} is an anagram of {}".format("".join(solution), phrase.full)
+        #could make solution an instance of phrase
+    
         
 class Phrase():
     def __init__(self, phrase):
+        #do some type checking
         self.full = phrase
         self.stripped = self._remove_spaces(self.full)
         self.dictionary = self._make_dictionary(self.stripped)
@@ -155,12 +198,8 @@ class Alphabet(Enum):
     
     
 if __name__ == "__main__":
-	import os, sys
-	if len(sys.argv) > 1:
-		path = os.path.join(*sys.argv[1:])
-	else:
-		path = "test_dictionary.txt"
-	anagram_dictionary = AnagramDictionary(path)
-	phrase = Phrase("redefined frights")
-	anagram_dictionary.find_anagrams(phrase)
-
+    import os
+    path = os.path.join("test_dictionary.txt")
+    anagram_dictionary = AnagramDictionary(path)
+    phrase = Phrase("redefined frights")
+    anagram_dictionary.find_anagrams(phrase)
--- test_anagrams.py
+++ test_anagrams.py
@@ -1,5 +1,5 @@
 import unittest
-from anagrams import AnagramDictionary, Phrase
+from anagrams import AnagramDictionary, Phrase, Node
 '''
 Created on Oct 26, 2015
 
@@ -12,41 +12,36 @@ class TestDictionary(unittest.TestCase):
         self.dictionary = AnagramDictionary()
     
     def test_insert_word(self):
+        self.dictionary.clear_dictionary()
         word = "friend"
         self.dictionary._insert_word(word)
         assert len(self.dictionary.node_list) == 7, "node list length = {}".format(len(self.dictionary.node_list))
-        assert self.dictionary.words == [None, None, None, None, None, None, "friend"], "dictionary.words = {}".format(self.dictionary.words)
-        assert self.dictionary.node_list == [{"f":1}, {"r":2}, {"i":3}, {"e":4}, {"n":5}, {"d":6}, {}], "node list = {}".format(self.dictionary.node_list)
-        assert self.dictionary.back_pointers == [None, 0, 1, 2, 3, 4, 5], "backpointers = {}".format(self.dictionary.back_pointers)
+        assert self.dictionary.node_list == [Node({"f":1}, None, None), Node({"r":2}, 0, None), Node({"i":3}, 1, None), Node({"e":4}, 2, None), Node({"n":5}, 3, None), Node({"d":6}, 4, None), Node({}, 5, "friend")], "node_list = {}".format(self.dictionary.node_list)     
         word = "freight"
         self.dictionary._insert_word(word)
         assert len(self.dictionary.node_list) == 12, "node list length = {}".format(len(self.dictionary.node_list))
-        assert self.dictionary.words == [None, None, None, None, None, None, "friend", None, None, None, None, "freight"], "dictionary.words = {}".format(self.dictionary.words)
-        assert self.dictionary.node_list == [{"f":1}, {"r":2}, {"i":3, "e":7}, {"e":4}, {"n":5}, {"d":6}, {}, {"i":8}, {"g":9}, {"h":10}, {"t":11}, {}], "node list = {}".format(self.dictionary.node_list)
-        assert self.dictionary.back_pointers == [None, 0, 1, 2, 3, 4, 5, 2, 7, 8, 9, 10], "backpointers = {}".format(self.dictionary.back_pointers)
+        assert self.dictionary.node_list == [Node({"f":1}, None, None), Node({"r":2}, 0, None), Node({"i":3, "e":7}, 1, None), Node({"e":4}, 2, None), Node({"n":5}, 3, None), Node({"d":6}, 4, None), Node({}, 5, "friend"), Node({"i":8}, 2, None), Node({"g":9}, 7, None), Node({"h":10}, 8, None), Node({"t":11}, 9, None), Node({}, 10, "freight")], "node_list = {}".format(self.dictionary.node_list)
         word = "friends"
         self.dictionary._insert_word(word)
         assert len(self.dictionary.node_list) == 13, "node list length = {}".format(len(self.dictionary.node_list))
-        assert self.dictionary.words == [None, None, None, None, None, None, "friend", None, None, None, None, "freight", "friends"], "dictionary.words = {}".format(self.dictionary.words)
-        assert self.dictionary.node_list == [{"f":1}, {"r":2}, {"i":3, "e":7}, {"e":4}, {"n":5}, {"d":6}, {"s":12}, {"i":8}, {"g":9}, {"h":10}, {"t":11}, {}, {}], "node list = {}".format(self.dictionary.node_list)
-        assert self.dictionary.back_pointers == [None, 0, 1, 2, 3, 4, 5, 2, 7, 8, 9, 10, 6], "backpointers = {}".format(self.dictionary.back_pointers)
+        assert self.dictionary.node_list == [Node({"f":1}, None, None), Node({"r":2}, 0, None), Node({"i":3, "e":7}, 1, None), Node({"e":4}, 2, None), Node({"n":5}, 3, None), Node({"d":6}, 4, None), Node({"s":12}, 5, "friend"), Node({"i":8}, 2, None), Node({"g":9}, 7, None), Node({"h":10}, 8, None), Node({"t":11}, 9, None), Node({}, 10, "freight"), Node({}, 6, "friends")], "node_list = {}".format(self.dictionary.node_list)
         word = "freighted"
         self.dictionary._insert_word(word)
-        assert len(self.dictionary.node_list) == 15, "node list length = {}".format(len(self.dictionary.node_list))
-        assert self.dictionary.words == [None, None, None, None, None, None, "friend", None, None, None, None, "freight", "friends", None, "freighted"], "dictionary.words = {}".format(self.dictionary.words)
-        assert self.dictionary.node_list == [{"f":1}, {"r":2}, {"i":3, "e":7}, {"e":4}, {"n":5}, {"d":6}, {"s":12}, {"i":8}, {"g":9}, {"h":10}, {"t":11}, {"e":13}, {}, {"d":14}, {}], "node list = {}".format(self.dictionary.node_list)
+        assert self.dictionary.node_list == [Node({"f":1}, None, None), Node({"r":2}, 0, None), Node({"i":3, "e":7}, 1, None), Node({"e":4}, 2, None), Node({"n":5}, 3, None), Node({"d":6}, 4, None), Node({"s":12}, 5, "friend"), Node({"i":8}, 2, None), Node({"g":9}, 7, None), Node({"h":10}, 8, None), Node({"t":11}, 9, None), Node({"e":13}, 10, "freight"), Node({}, 6, "friends"), Node({"d":14}, 11, None), Node({}, 13, "freighted")], "node_list = {}".format(self.dictionary.node_list)
+        assert len(self.dictionary.node_list) == 15, "node list length = {}".format(len(self.dictionary.node_list)) 
         word = "frei"
         self.dictionary._insert_word(word)
         assert len(self.dictionary.node_list) == 15, "node list length = {}".format(len(self.dictionary.node_list))
-        assert self.dictionary.words == [None, None, None, None, None, None, "friend", None, "frei", None, None, "freight", "friends", None, "freighted"], "dictionary.words = {}".format(self.dictionary.words)
-        assert self.dictionary.node_list == [{"f":1}, {"r":2}, {"i":3, "e":7}, {"e":4}, {"n":5}, {"d":6}, {"s":12}, {"i":8}, {"g":9}, {"h":10}, {"t":11}, {"e":13}, {}, {"d":14}, {}], "node list = {}".format(self.dictionary.node_list)
+        assert self.dictionary.node_list == [Node({"f":1}, None, None), Node({"r":2}, 0, None), Node({"i":3, "e":7}, 1, None), Node({"e":4}, 2, None), Node({"n":5}, 3, None), Node({"d":6}, 4, None), Node({"s":12}, 5, "friend"), Node({"i":8}, 2, None), Node({"g":9}, 7, "frei"), Node({"h":10}, 8, None), Node({"t":11}, 9, None), Node({"e":13}, 10, "freight"), Node({}, 6, "friends"), Node({"d":14}, 11, None), Node({}, 13, "freighted")], "node_list = {}".format(self.dictionary.node_list)   
+        print ("finished insert word test")
         
     def test_set_dictionary(self):
         path = "test_dictionary.txt"
         self.dictionary.set_dictionary(path)
         assert len(self.dictionary.node_list) == 15, "node list length = {}".format(len(self.dictionary.node_list))
-        assert self.dictionary.words == [None, None, None, None, None, None, "friend", None, None, None, None, "freight", "friends", None, "freighted"], "dictionary.words = {}".format(self.dictionary.words)
-        assert self.dictionary.node_list == [{"f":1}, {"r":2}, {"i":3, "e":7}, {"e":4}, {"n":5}, {"d":6}, {"s":12}, {"i":8}, {"g":9}, {"h":10}, {"t":11}, {"e":13}, {}, {"d":14}, {}], "node list = {}".format(self.dictionary.node_list)
+        assert self.dictionary.node_list == [Node({"f":1}, None, None), Node({"r":2}, 0, None), Node({"i":3, "e":7}, 1, None), Node({"e":4}, 2, None), Node({"n":5}, 3, None), Node({"d":6}, 4, None), Node({"s":12}, 5, "friend"), Node({"i":8}, 2, None), Node({"g":9}, 7, None), Node({"h":10}, 8, None), Node({"t":11}, 9, None), Node({"e":13}, 10, "freight"), Node({}, 6, "friends"), Node({"d":14}, 11, None), Node({}, 13, "freighted")], "node_list = {}".format(self.dictionary.node_list)
+        #assert self.dictionary.node_list == [None, None, None, None, None, None, "friend", None, None, None, None, "freight", "friends", None, "freighted"], "dictionary.words = {}".format(self.dictionary.words)
+        #assert self.dictionary.node_list == [{"f":1}, {"r":2}, {"i":3, "e":7}, {"e":4}, {"n":5}, {"d":6}, {"s":12}, {"i":8}, {"g":9}, {"h":10}, {"t":11}, {"e":13}, {}, {"d":14}, {}], "node list = {}".format(self.dictionary.node_list)
        
     def test_phrase(self):
         import operator
@@ -65,6 +60,7 @@ class TestDictionary(unittest.TestCase):
         assert self.dictionary._is_solution(phrase, solution) == False
 
     def test_find_anagrams(self):
+        print ("testing find_anagrams")
         path = "test_dictionary.txt"
         self.dictionary.set_dictionary(path)
         phrase = Phrase("redefined frights")
@@ -75,4 +71,3 @@ class TestDictionary(unittest.TestCase):
 #test set_dictionary (additive)
 #test set_dictionary (reset)
 #should address upper/lower case
-            
